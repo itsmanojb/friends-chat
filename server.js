ec4bc11e5b162841efa86f8dd7d04d157d86d134
itsmanojb <the_manoj@live.com>
2020-05-17 21:07:45 +0530

    :sparkles: Peer to peer calling

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-
 const server = http.createServer(app);
 const socket = require('socket.io');
 const io = socket(server);
 
 const users = {};
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   if (!users[socket.id]) {
     users[socket.id] = socket.id;
   }
-  socket.emit('Your Id:', socket.id);
-  io.sockets.emit('All users', users);
+  socket.emit('yourID', socket.id);
+  io.sockets.emit('allUsers', users);
   socket.on('disconnect', () => {
     delete users[socket.id];
-  });
+  })
 
   socket.on('callUser', (data) => {
     io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
-  });
+  })
 
-  socket.on('acceptCall', () => {
+  socket.on('acceptCall', (data) => {
     io.to(data.to).emit('callAccepted', data.signal);
-  });
-
+  })
 });
 
-server.listen(8000, () => console.log(`Server running on port: 8000`));
+server.listen(8000, () => console.log('server is running on port 8000'));
+
