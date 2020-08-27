8d88eb07b0dc459b0c9bd24d1d9763a0acf4e728
itsmanojb <the_manoj@live.com>
2020-08-28 02:07:33 +0530

    core func - done

--- public/script.js
+++ public/script.js
@@ -1,21 +1,100 @@
+const socket = io('/');
 const videoGrid = document.getElementById('videoGrid');
+const chatBox = document.getElementById('chatBox');
 const myVideo = document.createElement('video');
-myVideo.muted = true;
+// myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
 
 let videoStream;
-
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then((stream) => {
-  videoStream = stream;
-  addVideoStream(myVideo, stream);
-})
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
+    video.play();
   });
   videoGrid.append(video);
-}
+};
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    videoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+peer.on('open', (peerId) => {
+  socket.emit('join-room', roomId, peerId);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+const input = document.getElementById('messageBox');
+input.addEventListener('keydown', ({ key }) => {
+  if (key === 'Enter') {
+    event.preventDefault();
+    const value = input.value.trim();
+    if (value) {
+      socket.emit('message', value);
+      input.value = '';
+    }
+  }
+});
+
+socket.on('create-message', (message) => {
+  const newMessage = `<div>${message}</div>`;
+  chatBox.innerHTML = chatBox.innerHTML + newMessage;
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  const element = document.getElementById('chatBox');
+  element.scrollTop = element.scrollHeight - element.clientHeight;
+};
+
+const toggleMute = () => {
+  const enabled = videoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    videoStream.getAudioTracks()[0].enabled = false;
+    document.getElementById('muteBtn').innerText = 'Unmute';
+  } else {
+    videoStream.getAudioTracks()[0].enabled = true;
+    document.getElementById('muteBtn').innerText = 'Mute';
+  }
+};
+
+const toggleVideo = () => {
+  const enabled = videoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    videoStream.getVideoTracks()[0].enabled = false;
+    document.getElementById('videoBtn').innerText = 'Play Video';
+  } else {
+    videoStream.getVideoTracks()[0].enabled = true;
+    document.getElementById('videoBtn').innerText = 'Stop Video';
+  }
+};
--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const {
-  v4: uuidv4
-} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`)
+  res.redirect(`/${uuidv4()}`);
 });
 
 app.get('/:room', (req, res) => {
   res.render('room', {
-    id: req.params.room
-  })
+    id: req.params.room,
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.on('message', (message) => {
+      io.to(roomId).emit('create-message', message);
+    });
+  });
 });
 
 server.listen(3030);
