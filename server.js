c0e7298b8f9b7804a74039830ee5f5512f165fb2
itsmanojb <the_manoj@live.com>
2020-08-27 22:07:54 +0530

    :sparkles: Server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello')
+});
+
+server.listen(3030);
